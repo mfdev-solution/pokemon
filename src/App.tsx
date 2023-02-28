b7e2eb682834b663b921f8c3589b72404e9d2490
mfdev-solution <lasolution.mfdev@gmail.com>
2023-02-28 10:36:16 +0000

    ajout de pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-  
-const App: React.FC = () => {
- const name: String = 'React';
-    
+import React ,{FunctionComponent,useState} from 'react';
+import Pokemon from './model/pokemon';
+import POKEMONS from './model/mock-pokemon';
+
+
+
+const App: FunctionComponent = () => {
+ const [pokemons]  = useState<Pokemon[]>(POKEMONS)
+
  return (
-  <h1>Hello, {name} !</h1>
+  <div>
+    <h1>Pokedex</h1>
+    <p>Il y a {pokemons.length} pokemons</p>
+  </div>
  )
 }
   
