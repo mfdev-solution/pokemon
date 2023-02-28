b27d7781c2b4d712e29abd53303a5ef87979263c
mfdev-solution <lasolution.mfdev@gmail.com>
2023-02-28 13:11:06 +0000

    styling the showing of the pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import React ,{FunctionComponent,useState,useEffect} from 'react';
-import Pokemon from './model/pokemon';
-import POKEMONS from './model/mock-pokemon';
+import React ,{FunctionComponent} from 'react';
+import PokemonList from './pages/pokemon-list';
 
 
 
 const App: FunctionComponent = () => {
- const [pokemons,setPokemons]  = useState<Pokemon[]>([])
 
-useEffect(()=>{
-    setPokemons(POKEMONS)
-},[]);
  return (
   <div>
-    <h1 className='center'>Pokedex</h1>
-    <div className="container">
-        <div className="row">
-            {pokemons.map(({id,name,picture,created})=>(
-                <div className="col s6 m4" key={id}>
-                    <div className="card horizontal">
-                        <div className="card-image">
-                            <img src={picture} alt={name} />
-                        </div>
-                        <div className="card-stacked">
-                            <div className="card-content">
-                                <p>{name}</p>
-                                <p><small>{created.toString()}</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    </div>
+   <PokemonList />
   </div>
  )
 }
