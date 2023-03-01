d8daa24626caff60b2715a7d17af81958ed61681
mfdev-solution <lasolution.mfdev@gmail.com>
2023-03-01 11:35:04 +0000

    handling the errors

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -4,16 +4,22 @@ export default class PokemonService {
  
   static getPokemons(): Promise<Pokemon[]> {
     return fetch('http://localhost:3001/pokemons')
-      .then(response => response.json());
+      .then(response => response.json())
+      .catch(err => this.handleError(err))
   }
  
   static getPokemon(id: number): Promise<Pokemon|null> {
     return fetch(`http://localhost:3001/pokemons/${id}`)
       .then(response => response.json())
-      .then(data => this.isEmpty(data) ? null : data);
+      .then(data => this.isEmpty(data) ? null : data)
+      .catch(err => this.handleError(err));
   }
  
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
+  }
+  static handleError(err: Error): void {
+    console.log(err);
+    
   }
 }
