c3fd4eaf77d831674708b27c6079b3296e2d0750
mfdev-solution <lasolution.mfdev@gmail.com>
2023-03-03 08:22:30 +0000

    change the login and the password

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -22,7 +22,7 @@ const Login: FunctionComponent = () => {
     password: { value: '' },
   });
 
-  const [message, setMessage] = useState<string>('Vous êtes déconnecté. (pikachu / pikachu)');
+  const [message, setMessage] = useState<string>('Vous êtes déconnecté. (fall / passer)');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const fieldName: string = e.target.name;
